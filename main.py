9df491e7881fca4abc0198b5b8596af6f4440450
harshdhuppad05 <harshdhuppad05@gmail.com>
2025-05-08 16:31:02 +0530

    made a basic chatbot workflow

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ class State(TypedDict):
     messages: Annotated[str, add_messages]
 
 graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messages" : [llm.invoke(state['messages'])]}
+
+graph_builder.add_node("Chatbot",chatbot)
+
+graph_builder.add_edge(START, "Chatbot")
+graph_builder.add_edge("Chatbot", END)
+
+graph = graph_builder.compile()
+
+user_input = input("Enter your message: ")
+state = graph.invoke({"messages":[{"role": "user", "content": user_input}]})
+
+print(state['messages'][-1].content)
